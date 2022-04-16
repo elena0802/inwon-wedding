af93e5e77e76665117f4de27f85d24bb9a7451a2
elena0802 <101960873+elena0802@users.noreply.github.com>
2022-04-15 22:31:09 -0700

    countdown update

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ $(document).ready(function() {
 	// 07. COUNTDOWN
 	//===================================================================================
 	var theday = new Date();
-	theday = new Date(2022, 5, 29);
+	theday = new Date(2022, 4, 29);
 		 
 	$('#countdown').countdown({until: theday, format: 'WDHMS'});
 	$('#countdown').countdown($.countdown.regionalOptions['custom-label']); 
